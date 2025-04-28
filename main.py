ee28f78994e1e6e4c5bbd9790526dfe2cba9314b
A-Infor <anderson.informatica.mg@gmx.com>
2025-04-27 21:59:29 -0300

    `performance_evaluator.py`: -nasty bug; `main.py`: +dual model/cluster mode
    The nasty bug on `performance_evaluator.py` was caused by `self.metrics_df` being defined inside `evaluate()`, instead of `__init__()`.
    
    The big improvement on `main.py` is the showcase of the ability of handling more than one model/city cluster at once.

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from classes import Dataset, NeuralNetwork, Plotter, PerformanceEvaluator
+import pandas     as pd
+###############################################################################
+# CITY CLUSTER 1:
+## Central city  : RIO PARDO DE MINAS
+rio_pardo_de_mg_dataset = Dataset       ('Rio Pardo de Minas', 'Rio Pardo de Minas', './Data/cluster RIO PARDO DE MINAS/', 'RIO PARDO DE MINAS.xlsx')
+rio_pardo_de_mg_plotter = Plotter       (rio_pardo_de_mg_dataset)
+rio_pardo_de_mg_model   = NeuralNetwork ('config.json', rio_pardo_de_mg_dataset, rio_pardo_de_mg_plotter)
 
-rio_pardo_de_mg_dataset   = Dataset       ('Rio Pardo de Minas', 'Rio Pardo de Minas', './Data/', 'RIO PARDO DE MINAS.xlsx')
-rio_pardo_de_mg_plotter   = Plotter       (rio_pardo_de_mg_dataset)
-rio_pardo_de_mg_model     = NeuralNetwork ('config.json', rio_pardo_de_mg_dataset, rio_pardo_de_mg_plotter)
+rio_pardo_de_mg_metrics_df = rio_pardo_de_mg_model.use_neural_network ()
+rio_pardo_de_mg_plotter.plotMetricsPlots              (rio_pardo_de_mg_metrics_df)
 
-metrics_df = rio_pardo_de_mg_model.use_neural_network ()
-rio_pardo_de_mg_plotter.plotMetricsPlots              (metrics_df)
+## Bordering city: MONTEZUMA
+montezuma_dataset       = Dataset ('Montezuma', 'Rio Pardo de Minas', './Data/cluster RIO PARDO DE MINAS/', 'MONTEZUMA.xlsx')
+montezuma_plotter       = Plotter (montezuma_dataset)
 
-montezuma_dataset         = Dataset ('Montezuma', 'Rio Pardo de Minas', './Data/', 'MONTEZUMA.xlsx')
-montezuma_plotter         = Plotter (montezuma_dataset)
+rio_pardo_de_mg_metrics_df = rio_pardo_de_mg_model.use_neural_network (dataset=montezuma_dataset, plotter=montezuma_plotter)
+montezuma_plotter.plotMetricsPlots                 (rio_pardo_de_mg_metrics_df)
+###############################################################################
+# CITY CLUSTER 2:
+## Central city  : SÃO FRANCISCO
+sao_francisco_dataset = Dataset       ('São Francisco', 'São Francisco', './Data/cluster SÃO FRANCISCO/', 'SÃO FRANCISCO.xlsx')
+sao_francisco_plotter = Plotter       (sao_francisco_dataset)
+sao_francisco_model   = NeuralNetwork ('config.json', sao_francisco_dataset, sao_francisco_plotter)
 
-metrics_df = rio_pardo_de_mg_model.use_neural_network (dataset=montezuma_dataset, plotter=montezuma_plotter)
-montezuma_plotter.plotMetricsPlots                 (metrics_df)
+sao_francisco_metrics_df = sao_francisco_model.use_neural_network ()
+sao_francisco_plotter.plotMetricsPlots              (sao_francisco_metrics_df)
+
+## Bordering city: PINTÓPOLIS
+pintopolis_dataset    = Dataset ('Pintópolis', 'São Francisco', './Data/cluster SÃO FRANCISCO/', 'PINTÓPOLIS.xlsx')
+pintopolis_plotter    = Plotter (pintopolis_dataset)
+
+sao_francisco_metrics_df = sao_francisco_model.use_neural_network (dataset=pintopolis_dataset, plotter=pintopolis_plotter)
+pintopolis_plotter.plotMetricsPlots                 (sao_francisco_metrics_df)
+###############################################################################
+# METRICS RESULTS JOINING:
+metrics_df = pd.concat([rio_pardo_de_mg_metrics_df, sao_francisco_metrics_df], ignore_index=True)
+pintopolis_plotter.drawMetricsBoxPlots(metrics_df)
+pintopolis_plotter.drawMetricsBarPlots(metrics_df)
+###############################################################################
